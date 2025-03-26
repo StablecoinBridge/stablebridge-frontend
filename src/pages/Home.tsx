d44c113982198d5c6585435156ad31dee2f648e0
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-03-26 09:54:40 +0100

    Add hero gradient background matching design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,31 +18,17 @@ const Home = () => {
   }, []);
 
   return (
-    
-    // <motion.div
-    //   initial={{ opacity: 0 }}
-    //   animate={{ opacity: 1 }}
-    //   exit={{ opacity: 0 }}
-    //   className="min-h-screen bg-[#090909] text-white"
-    // >
-    //   <Navbar />
-    //   <Hero />
-    //   <Stats />
-    //   <Mission />
-    //   <Developers />
-    // </motion.div>
     <motion.div 
-      className="min-h-screen bg-allbridge-black overflow-hidden relative"
+      className="min-h-screen bg-hero-gradient overflow-hidden"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="w-full h-8 bg-mint"></div>
-      <div className="bg-black">
-        <Navbar />
-        <Hero />
-      </div>
-      
+      <Navbar />
+      <Hero />
+      <Stats />
+      <Mission />
+      <Developers />
     </motion.div>
   );
 };
