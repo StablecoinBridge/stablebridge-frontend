6e4fcd6281f0061e1564e14f31836677b0b19004
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-03-28 22:27:44 +0100

    explorer

--- src/pages/Explorer.tsx
+++ src/pages/Explorer.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import Header from '@/components/liquidity/Header'
-const Explorer = () => {
-  return (
-    <div>
-        <Header darkMode={false} toggleDarkMode={() => {}} />
-    </div>
-  )
-}
+import Header from "@/components/liquidity/Header";
+import React, { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import PoolsHeader from "@/components/liquidity/PoolsHeader";
+import { Search } from "lucide-react";
+const Explorer: React.FC = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsScrolled(scrollPosition > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-export default Explorer
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-b from-white to-purple-300 text-foreground flex flex-col">
+        <Header />
+        <div
+          className={cn(
+            "container max-w-5xl px-4 md:px-6 lg:px-8 py-4 md:py-8 mx-auto transition-all duration-300",
+            isScrolled ? "mt-[72px]" : ""
+          )}
+        >
+          <PoolsHeader title="Explorer" icon={<Search className="text-violet-600" />} />
+
+          <div className="grid grid-cols-1 gap-6 mx-auto"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Explorer;
