b877cc0a7573262c5128412ea805cf8eff8b11c6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-03-28 22:13:15 +0100

    liquidity

---
+++ src/pages/Explorer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '@/components/liquidity/Header'
+const Explorer = () => {
+  return (
+    <div>
+        <Header darkMode={false} toggleDarkMode={() => {}} />
+    </div>
+  )
+}
+
+export default Explorer
